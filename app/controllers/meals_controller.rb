cf3607c04bd3af03c98f4f92db8cbfe8027369ba
mat <mat@matyou.net>
2019-07-23 14:24:58 -0600

    check

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,38 +1,38 @@
 class MealsController < ApplicationController
   before_action :loged_in
   def index
-    begin
+    # begin
       url = ENV['all_categories_url']
       result = Net::HTTP.get(URI.parse(url))
       categories = JSON.parse(result)
       @categories = categories['categories']
     rescue
-      @error = 'Sorry: Meal Server Down. Please Come Back Later!'
-    end
+      # @error = 'Sorry: Meal Server Down. Please Come Back Later!'
+    # end
   end
 
   def meals
     @likes = current_user.likes.map { |l| l.meal_string }
-    begin
+    # begin
       url = ENV['category_url'] + params[:category]
       result = Net::HTTP.get(URI.parse(url))
       meals = JSON.parse(result)
       @meals = meals['meals']
-    rescue
-      @error = 'Sorry: Meal Server Down. Please Come Back Later!'
-    end
+    # rescue
+      # @error = 'Sorry: Meal Server Down. Please Come Back Later!'
+    # end
      render partial: 'meals'
   end
 
   def recipes
-    begin
+    # begin
       url = ENV['meal_details_url'] + params[:id]
       result = Net::HTTP.get(URI.parse(url))
       recipe = JSON.parse(result)
       @recipe = recipe['meals'][0]
-    rescue
-      @error = 'Sorry: Meal Server Down. Please Come Back Later!'
-    end
+    # rescue
+      # @error = 'Sorry: Meal Server Down. Please Come Back Later!'
+    # end
     render partial: 'show_recipe'
   end
 
