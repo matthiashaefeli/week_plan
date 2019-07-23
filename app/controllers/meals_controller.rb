6783691153c79ad73ee2e722cea430dbe88fd070
mat <mat@matyou.net>
2019-07-23 14:00:24 -0600

    change var

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -2,7 +2,7 @@ class MealsController < ApplicationController
   before_action :loged_in
   def index
     begin
-      url = ENV['ALL_CAT_URL']
+      url = ENV['all_categories_url']
       result = Net::HTTP.get(URI.parse(url))
       categories = JSON.parse(result)
       @categories = categories['categories']
@@ -14,7 +14,7 @@ class MealsController < ApplicationController
   def meals
     @likes = current_user.likes.map { |l| l.meal_string }
     begin
-      url = ENV['CAT_URL'] + params[:category]
+      url = ENV['category_url'] + params[:category]
       result = Net::HTTP.get(URI.parse(url))
       meals = JSON.parse(result)
       @meals = meals['meals']
@@ -26,7 +26,7 @@ class MealsController < ApplicationController
 
   def recipes
     begin
-      url = ENV['MEAL_DETAILS_URL'] + params[:id]
+      url = ENV['meal_details_url'] + params[:id]
       result = Net::HTTP.get(URI.parse(url))
       recipe = JSON.parse(result)
       @recipe = recipe['meals'][0]
