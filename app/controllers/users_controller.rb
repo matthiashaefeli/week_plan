962e33cf8b40447afb83db741d9397d400cf6cdc
mat <mat@matyou.net>
2019-07-23 21:47:34 -0600

    email downcase

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to login_path, notice: 'User was successfully created.' }
+        format.html { redirect_to login_path, notice: ['User was successfully created.'] }
       else
         format.html { redirect_to new_user_path, notice: @user.errors.full_messages }
       end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :email, presence: true, uniqueness: true
   validates_format_of :email, :with => /@/
   validates :user_name, presence: true, uniqueness: true
   validates :password, presence: true, length: { within: 4..40 }
+
+  before_save :downcase_fields
+
   has_many :likes
   has_many :meals
+
+  def downcase_fields
+    self.email.downcase!
+  end
 end
