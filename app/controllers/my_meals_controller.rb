50c68c57cd45b0e55c7772c1cf89a8bd52427cc5
mat <mat@matyou.net>
2019-07-29 12:12:22 -0600

    edit my meal

--- app/controllers/my_meals_controller.rb
+++ app/controllers/my_meals_controller.rb
@@ -19,6 +19,23 @@ class MyMealsController < ApplicationController
     end
   end
 
+  def update
+    my_meal = MyMeal.find(params[:id])
+    my_meal.update_attributes(my_meal_params)
+    if my_meal.avatar.attached?
+      my_meal.strMealThumb = url_for(my_meal.avatar)
+    end
+    if my_meal.save
+      redirect_to my_meals_path
+    else
+      render :edit, locals: { notice: my_meal.errors.full_messages }
+    end
+  end
+
+  def edit
+    @my_meal = MyMeal.find(params[:id])
+  end
+
   def destroy
     my_meal = MyMeal.find(params[:id])
     my_meal.destroy
